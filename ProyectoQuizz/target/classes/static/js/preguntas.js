13002ccacdfe8059d1bc5975759a408f6bcbc7ef
Ari558 <69749025+Ari558@users.noreply.github.com>
2020-12-14 00:02:18 -0600

    Estilos

---
+++ ProyectoQuizz/target/classes/static/js/preguntas.js
@@ -0,0 +1,105 @@
+
+//Arreglo de objetos
+const quiz = [
+    {
+        q:'¿Qué significa instanciar una clase?',
+        options:['Duplicar una clase','Eliminar la clase','Crear un objeto a partir de la clase','Conectar dos clases entre sí'],
+        answer:2
+    },
+    {
+        q:'¿Qué es Eclipse?',
+        options:['Una librería de Java','Una versión de Java especial para servidores','Un IDE para desarrollar aplicaciones','Ninguna de las anteriores'],
+        answer:2
+    },
+    {
+        q:'¿Qué elementos definen a un objeto?',
+        options:['Sus cardinales y tipos','Sus Atributos y métodos','Su interfaz  y los eventos asociados','Ninguna de las anteriores'],
+        answer:1
+    },
+    {
+        q:'¿Cuál de estos es un operador lógico de Java?',
+        options:['*','/','!=','**'],
+        answer:2
+    },
+    {
+        q:'En Java, cada línea de código que se puede ejecutar necesita estar dentro de:',
+        options:['Un public','Un objeto','Una clase','Un String'],
+        answer:2
+    },
+    {
+        q:'Seleccione el método que no retorna ningún valor en Java',
+        options:['return','main','void','static'],
+        answer:2
+    },
+    {
+        q:'En las clases, métodos y otras estructuras de control de flujo, el código siempre está delimitado con',
+        options:[';',':','[]','{}'],
+        answer:3
+    },
+    {
+        q:'¿Cúal es el método que imprime un texto en un programa Java?',
+        options:['System.out.println()','out.write.Text()','System.out()','System.print.Text()'],
+        answer:0
+    },
+    {
+        q:'¿Cuál es el nombre del creador de Java?',
+        options:['Linus Torvalds','Brian Kernighan','James Gosling','Anders Heljlsberg'],
+        answer:2
+    },
+    {
+        q:'¿Qué significa las siglas JVM?',
+        options:['Java Virtual Machine','Java Viral Motors','Java Virtual Magnament','Ninguna de las anteriores'],
+        answer:0
+    },
+    {
+        q:'¿Para qué se utilizan los get y set?',
+        options:['Se utilizan para darle un valor a la variable','Métodos utilizados en las clases para mostrar o modificar el valor de un atributo','Se encargan de convertir cadenas de texto a objetos','Ninguna de las anteriores'],
+        answer:1
+    },
+    {
+        q:'¿Para que se utiliza la condicional If?',
+        options:['Para evaluar y tomar decisiones','Para realizar ciertas operaciones sin devolver un valor','Para declarar un objeto de una clase','Ninguna de las anteriores'],
+        answer:0
+    },
+    {
+        q:'Java es la abreviatura de "JavaScript"',
+        options:['Si','No'],
+        answer:1
+    },
+    {
+        q:'¿Cómo se insertan COMENTARIOS en el código Java?',
+        options:['# Este es un comentario','// Este es un comentario','/* Este es un comentario','-- Este es un comentario'],
+        answer:1
+    },
+    {
+        q:'¿Qué tipo de datos se usa para crear una variable que debería almacenar texto?',
+        options:['Txt','String','myString','text'],
+        answer:1
+    },
+    {
+        q:'¿Cómo se crea una variable con el valor numérico 5?',
+        options:['int x = 5;','num x = 5','x = 5;','float x = 5;'],
+        answer:0
+    },
+    {
+        q:'¿Qué método se puede usar para encontrar la longitud de una cadena?',
+        options:['getLength()','len()','length()','getSize()'],
+        answer:2
+    },
+    {
+        q:'Para declarar una matriz en Java, se define el tipo de variable con:',
+        options:['()','[]','{}'],
+        answer:1
+    },
+    {
+        q:'Los índices de matriz comienzan con:',
+        options:['1','0'],
+        answer:1
+    },
+    {
+        q:'¿Cómo se crea un método en Java?',
+        options:['nombreMétodo()','nombreMétodo[]','(nombreMétodo)','nombreMétodo.'],
+        answer:0
+    }
+
+]

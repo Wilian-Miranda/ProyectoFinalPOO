1c41055149c8c4dfaee8fa15a2f82ccca882608d
Wilian-Miranda <69541095+Wilian-Miranda@users.noreply.github.com>
2020-12-13 20:55:13 -0600

    Resubiendo

---
+++ ProyectoQuizz/src/main/java/com/unab/Quizz/ProyectoQuizzApplication.java
@@ -0,0 +1,13 @@
+package com.unab.Quizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoQuizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoQuizzApplication.class, args);
+	}
+
+}
